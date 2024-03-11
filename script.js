adf16d90b942c1d8bc8751aa35a6a98d16bba691
mehrnazkiani24 <mehrnaz03@yahoo.com>
2024-03-10 17:31:04 -0700

    updated password qualification, special charactors and numbers

--- script.js
+++ script.js
@@ -15,7 +15,32 @@ function generatePassword (){
   if (passwordlength<8 || passwordlength>126 || isNaN(passwordlength)){
     return "Please enter Password length between 8 and 126"
   }
+  var yesUpperCase = confirm("Do you like uppercase letters in your password")
+  var yesLowerCase = confirm("Do you like lowercase letters in your password")
+  var yesSnumbers = confirm("Do you like numbers in your password")
+  var yesSpecialcharactors = confirm("Do you like to have special charactors in your password")
+
+  if(!yesLowerCase && !yesSnumbers && !yesUpperCase &&!yesSpecialcharactors){
+    return "Please choose at least one option"
+  }
+
+  var validPasswords =""
+
+  if(yesLowerCase){
+    validPasswords += "abcdefghijklmnopqrstuvwxyz"
+  }
+  if(yesUpperCase){
+    validPasswords += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  }
+  if(yesSnumbers){
+    validPasswords += "0123456789"
+  }
+  if(yesSpecialcharactors){
+    validPasswords += "!#@$%^&*~_-"
+  }
+  console.log (validPasswords)
 }
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
